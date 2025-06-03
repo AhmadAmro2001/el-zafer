df9902098a6f814264ccfbdc15f7d94dec1978ae
Ahmad Amr <gamerteacher12@gmail.com>
2025-06-04 00:38:10 +0300

    4th commit

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -9,64 +9,126 @@ export default function NavBar() {
   return (
     <>
       <nav className="bg-white shadow-md">
-      <div className="max-w-screen-xl mx-auto px-4 py-3 flex justify-between items-center">
-        {/* Logo */}
-        <Link to="/" className="flex ">
-          <img src={zaferLogo} alt="El Zafer Logo" className="h-12   md:h-24" />
-        </Link>
-
-        {/* Hamburger Button */}
-        <button
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="lg:hidden text-gray-700 focus:outline-none"
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d={menuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+        <div className="max-w-screen-xl mx-auto px-4 py-3 flex justify-between items-center">
+          {/* Logo */}
+          <Link to="/" className="flex ">
+            <img
+              src={zaferLogo}
+              alt="El Zafer Logo"
+              className="h-12   md:h-24"
             />
-          </svg>
-        </button>
+          </Link>
 
-        {/* Navigation Items */}
-        <ul
-          className={`lg:flex gap-4 items-center ${
-            menuOpen ? "block mt-4" : "hidden"
-          } lg:mt-0`}
-        >
-          {[
-            "location-dot",
-            "facebook-f",
-            "linkedin-in",
-            "instagram",
-            "x-twitter",
-            "youtube",
-            "tiktok",
-          ].map((icon, idx) => (
-            <li key={idx}>
-              <NavLink to="#" className="block p-0">
+          {/* Hamburger Button */}
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="lg:hidden text-gray-700 focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={
+                  menuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"
+                }
+              />
+            </svg>
+          </button>
+
+          {/* Navigation Items */}
+          <ul
+            className={`lg:flex gap-4 items-center ${
+              menuOpen ? "block mt-4" : "hidden"
+            } lg:mt-0`}
+          >
+            <li>
+              <NavLink to="https://g.co/kgs/H1uVv2S" className="block p-0">
                 <div className="flex justify-center w-10 h-10 items-center my-2 md:my-0 border border-red-700 main-gradient text-white rounded-full">
-                  <i
-                    className={`${
-                      icon === "location-dot"
-                        ? "fa-solid fa-location-dot"
-                        : `fa-brands fa-${icon}`
-                    }`}
-                  ></i>
+                  <i className="fa-solid fa-location-dot"></i>
                 </div>
               </NavLink>
             </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+            <li>
+              <NavLink
+                to="https://www.facebook.com/alzafercargoKSA"
+                className="block p-0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex justify-center w-10 h-10 items-center my-2 md:my-0 border border-red-700 main-gradient text-white rounded-full">
+                  <i className="fa-brands fa-facebook-f"></i>
+                </div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="https://www.linkedin.com/company/al-zaferwingscargoservicescompany"
+                className="block p-0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex justify-center w-10 h-10 items-center my-2 md:my-0 border border-red-700 main-gradient text-white rounded-full">
+                  <i className="fa-brands fa-linkedin-in"></i>
+                </div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="https://www.instagram.com/sa.alzafer/"
+                className="block p-0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex justify-center w-10 h-10 items-center my-2 md:my-0 border border-red-700 main-gradient text-white rounded-full">
+                  <i className="fa-brands fa-instagram"></i>
+                </div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="https://x.com"
+                className="block p-0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex justify-center w-10 h-10 items-center my-2 md:my-0 border border-red-700 main-gradient text-white rounded-full">
+                  <i className="fa-brands fa-x-twitter"></i>
+                </div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="https://youtube.com"
+                className="block p-0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex justify-center w-10 h-10 items-center my-2 md:my-0 border border-red-700 main-gradient text-white rounded-full">
+                  <i className="fa-brands fa-youtube"></i>
+                </div>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="https://tiktok.com"
+                className="block p-0"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="flex justify-center w-10 h-10 items-center my-2 md:my-0 border border-red-700 main-gradient text-white rounded-full">
+                  <i className="fa-brands fa-tiktok"></i>
+                </div>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 }
