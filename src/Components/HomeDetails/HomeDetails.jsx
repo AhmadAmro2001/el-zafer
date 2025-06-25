dd9cfad44fc502552f4ad56ff6445b9306c6cd0d
Ahmad Amr <gamerteacher12@gmail.com>
2025-06-25 20:39:41 +0300

    26th

--- src/Components/HomeDetails/HomeDetails.jsx
+++ src/Components/HomeDetails/HomeDetails.jsx
@@ -6,13 +6,13 @@ export default function HomeDetails() {
   const { photo, title,desc} = state || {};
 return <>
       
-      <div className='container mx-auto px-4 md:px-36 mb-10 mt-44'>
+      <div className='container mx-auto px-4 md:px-36 mb-32 mt-10 md:mb-10 md:mt-44'>
         <div className='flex flex-col md:flex-row justify-between items-center'>
         <div className='text-left'>
           <h1 className='md:text-4xl text-[12px] mb-5 font-bold'>{title}</h1>
           <p className='md:text-[16px] text-[8px] max-w-[700px] text-gray-800'>{desc}</p>
         </div>
-        <div className='md:w-[150px]  w-[30px]  mt-8 md:mt-0  mx-auto'>
+        <div className='md:w-[150px]  w-[80px]  mt-8 md:mt-0  mx-auto'>
             <img
               src={photo}
               className="w-full  "
