c6cfb3fead04003fef7d0899bde761375c7133a1
Ahmad Amr <gamerteacher12@gmail.com>
2025-08-18 01:20:21 +0300

    last

--- src/Components/AboutUsWhy/AboutUsWhy.jsx
+++ src/Components/AboutUsWhy/AboutUsWhy.jsx
@@ -6,59 +6,32 @@ export default function AboutUsWhy() {
         
     }, [])
   return <>
-      {/* <div className='container mx-auto px-5 md:px-16 text-left py-5 md:py-16'>
-        <h1 className='font-semibold md:text-3xl text-[12px] mb-5'>Why Al Zafer?</h1>
-        <p className='text-[#999999] md:text-[16px] text-[8px]'>We offer years of professional experience within the freight forwarding, logistics and commercial storage sectors. Our portfolio of specialized services includes International Freight Forwarding for ocean and airfreight, Liner Service, Container Freight Station, Stevedoring & Terminal Operations, Customs Broking, Cargo Survey and Container Yard.
-        Our Company strives to gain the respect and ongoing support of our customers by offering high quality and value added service while maintaining the highest ethical standards. Our Objective is to perform beyond the expectations of all our customers in everything we do.</p>
-        <p className='text-[#999999] md:text-[16px] text-[8px]'>We offer years of professional experience within the freight forwarding, logistics and commercial storage sectors. Our portfolio of specialized services includes International Freight Forwarding for ocean and airfreight, Liner Service, Container Freight Station, Stevedoring & Terminal Operations, Customs Broking, Cargo Survey and Container Yard.
-        Our Company strives to gain the respect and ongoing support of our customers by offering high quality and value added service while maintaining the highest ethical standards. Our Objective is to perform beyond the expectations of all our customers in everything we do.</p>
-      </div> */}
+      
       {/* second part */}
       <div className='container mx-auto mt-5 md:mt-0 px-5 md:px-16 md:py-12'>
         <div className='flex flex-col md:flex-row justify-between items-center'>
-        <div className='text-left md:w-[55%] w-[100%] md:mr-16 '>
-          <p className='text-[#999999] mb-5 md:text-[16px] text-[8px]'>We offer years of professional experience within the freight forwarding, logistics and commercial storage sectors. Our portfolio of specialized services includes International Freight Forwarding for ocean and airfreight, Liner Service, Container Freight Station, Stevedoring & Terminal Operations, Customs Broking, Cargo Survey and Container Yard.
-          Our Company strives to gain the respect and ongoing support of our customers by offering high quality and value added service while maintaining the highest ethical standards. Our Objective is to perform beyond the expectations of all our customers in everything we do.</p>
-          <p className='text-[#999999]  md:text-[16px] text-[8px]'>We offer years of professional experience within the freight forwarding, logistics and commercial storage sectors. Our portfolio of specialized services includes International Freight Forwarding for ocean and airfreight, Liner Service, Container Freight Station, Stevedoring & Terminal Operations, Customs Broking, Cargo Survey and Container Yard.
-          Our Company strives to gain the respect and ongoing support of our customers by offering high quality and value added service while maintaining the highest ethical standards. Our Objective is to perform beyond the expectations of all our customers in everything we do.</p>
-        </div>
-        <div className='md:w-[45%] relative w-[100%] md:my-0 my-24'>
+        <div className='md:w-[45%] relative w-[100%] md:my-44 my-24 mx-auto'>
+
+
+
           <div className='md:w-[240px] md:h-[240px] w-[100px] h-[100px]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-ee-full bg-[#AC262F]'>
           </div>
+
+
+
           <div className='absolute md:top-0 right-20 rounded-3xl overflow-hidden md:right-8 md:w-[243px] md:h-[145px] w-[100px] h-[60px]'>
             <img src="https://res.cloudinary.com/djvzbznry/image/upload/v1751800756/IMG-20231003-WA0064_plnvb6.jpg" className='w-full h-full' alt="" />
           </div>
+
+
+
           <div className='absolute bottom-0 rounded-3xl overflow-hidden left-14 md:left-0 md:w-[319px] md:h-[179px] w-[120px] h-[70px]'>
             <img src="https://res.cloudinary.com/djvzbznry/image/upload/v1751800598/IMG-20231003-WA0071_oiub7v.jpg" className='w-full h-full' alt="" />
           </div>
-          {/* <div className='absolute md:w-[57px] md:h-[57px] w-[10px] h-[10px] rounded-full bg-black md:top-16 top-6 md:left-10 left-24'>
-          </div> */}
+          
         </div>
         </div>
       </div>
-      {/* third part */}
-      {/* <div className='container mx-auto px-5 md:px-16 md:py-12'>
-        <div className='flex flex-col md:flex-row justify-between items-center'>
-        <div className='md:w-[45%] relative w-[100%] md:my-0 my-24 hidden md:block'>
-          <div className='md:w-[240px] md:h-[240px] w-[100px] h-[100px]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-ee-full bg-[#0C71B9]'>
-          </div>
-          <div className='absolute md:top-0 right-20 rounded-3xl overflow-hidden md:right-10 md:w-[243px] md:h-[145px] w-[100px] h-[60px]'>
-            <img src="https://res.cloudinary.com/djvzbznry/image/upload/v1749697342/c4eb0b8de943f53e58b2c6740c31f68bf0981807_rpudzo.png" className='w-full h-full ' alt="" />
-          </div>
-          <div className='absolute bottom-0 rounded-3xl overflow-hidden left-14 md:left-0 md:w-[319px] md:h-[179px] w-[120px] h-[70px]'>
-            <img src="https://res.cloudinary.com/djvzbznry/image/upload/v1749697342/573da5926931f64278bf806f2e03365606af2c72_wxiilc.png" className='w-full h-full' alt="" />
-          </div>
-          <div className='absolute md:w-[57px] md:h-[57px] w-[10px] h-[10px] rounded-full bg-black md:top-16 top-6 md:left-10 left-24'>
-          </div>
-        </div>
-        <div className='text-left md:w-[55%] w-[100%] md:mr-16 mb-10 md:mb-0'>
-          <h1 className='font-semibold md:text-3xl text-[12px] mb-5'>Why Al Zafer?</h1>
-          <p className='text-[#999999] mb-5 md:text-[16px] text-[8px]'>We offer years of professional experience within the freight forwarding, logistics and commercial storage sectors. Our portfolio of specialized services includes International Freight Forwarding for ocean and airfreight, Liner Service, Container Freight Station, Stevedoring & Terminal Operations, Customs Broking, Cargo Survey and Container Yard.
-          Our Company strives to gain the respect and ongoing support of our customers by offering high quality and value added service while maintaining the highest ethical standards. Our Objective is to perform beyond the expectations of all our customers in everything we do.</p>
-          <p className='text-[#999999]  md:text-[16px] text-[8px]'>We offer years of professional experience within the freight forwarding, logistics and commercial storage sectors. Our portfolio of specialized services includes International Freight Forwarding for ocean and airfreight, Liner Service, Container Freight Station, Stevedoring & Terminal Operations, Customs Broking, Cargo Survey and Container Yard.
-          Our Company strives to gain the respect and ongoing support of our customers by offering high quality and value added service while maintaining the highest ethical standards. Our Objective is to perform beyond the expectations of all our customers in everything we do.</p>
-        </div>
-        </div>
-      </div> */}
+      
     </>
 }
