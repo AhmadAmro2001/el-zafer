9a7e268c201ae2e60de20ade8169d4b4632088b0
Ahmad Amr <gamerteacher12@gmail.com>
2025-08-24 10:40:06 +0300

    adjusts : about us again

--- src/Components/AboutUsCaption/AboutUsCaption.jsx
+++ src/Components/AboutUsCaption/AboutUsCaption.jsx
@@ -9,7 +9,7 @@ export default function AboutUsCaption() {
       <div className='container mx-auto px-5 md:px-32 text-left py-5 md:py-16'>
           
           <p className='text-gray-800 md:text-lg text-[8px]'>Al Zafer Wings Cargo Services Co. is an international freight forwarding outfit headquartered in Jeddah, which has represented excellence in the shipping, forwarding and logistics industry for almost four decades. We offer years of professional experience within the freight forwarding, logistics and commercial storage sectors.</p>
-          <p className='text-gray-800 md:text-lg text-[8px]'>Our portfolio of specialized services includes International Freight Forwarding for ocean and airfreight, Liner Service, Container Freight Station, Stevedoring & Terminal Operations, Customs Broking, Cargo Survey and Container Yard.</p>
+          
       </div>
     </>
 }
